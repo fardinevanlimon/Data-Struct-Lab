4395ba40c6bbf9dd66a7f8a12790631025f81814
Fardin Evan Limon <110395981+fardinevanlimon@users.noreply.github.com>
2022-10-13 13:48:45 +0600

    Create sizeof.c

---
+++ sizeof.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+int i;
+float c;
+double d;
+char f;
+printf("size of integer = %d\n",sizeof(i));
+printf("size of float = %d\n",sizeof(c));
+printf("size of double = %d\n",sizeof(d));
+printf("size of charecter = %d\n",sizeof(f));
+
+}
