d871ce964c5b1c5867fb66c3432fca777566440e
Fardin Evan Limon <110395981+fardinevanlimon@users.noreply.github.com>
2022-10-13 13:49:45 +0600

    Create array size.c

---
+++ array size.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+int A[5][5];
+float F[5][3];
+double D[3][3];
+char C[3][2];
+printf("size of integer = %d\n",sizeof(A));
+printf("size of float = %d\n",sizeof(F));
+printf("size of double = %d\n",sizeof(D));
+printf("size of charecter = %d\n",sizeof(C));
+
+}
