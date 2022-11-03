0063d357417d799d425d3f9a0092a168269cce29
Fardin Evan Limon <110395981+fardinevanlimon@users.noreply.github.com>
2022-11-03 15:30:22 +0600

    Create Linear.c

---
+++ Linear.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+    int main()
+    {
+       int list[20],size,i,sElement;
+
+                 printf("Enter size of the list: ");
+
+                 scanf("%d",&size);
+
+            ("Enter any %d integer values: ",size);
+
+             for(i = 0; i < size; i++)
+
+             scanf("%d",&list[i]);
+
+    printf("Enter the element to be Search: ");
+
+            scanf("%d",&sElement);
+
+    for(i = 0; i < size; i++)
+ {
+
+        if(sElement == list[i])
+    {
+
+        printf("Element is found at %d index", i);
+
+
+            break;
+            }
+        }
+                if(i == size)
+
+            printf("Given element is not found in the list!!!");
+
+
+
+            return 0;
+
+}
